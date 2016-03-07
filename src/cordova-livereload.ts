1a25d1f3062285963c56129a8cdbbd8f39754e1c
Carlos Santana <csantana23@gmail.com>
2016-03-06 22:57:05 -0500

    browsersync integration

--- src/cordova-livereload.ts
+++ src/cordova-livereload.ts
@@ -8,38 +8,60 @@ import ConfigXml = require('./ConfigXML');
 
 var exec = child_process.execSync;
 
+// Create a Browsersync instance
+import bSync = require("browser-sync");
+
+var bs = bSync.create();
+
 class CordovaLiveReload {
 
   public static run(): void {
     var platform: string;
+    var liveUrl: string;
+    var serverPath: string;
 
     if (process.argv.length < 3) {
       console.error("missing platform id ios or android");
       return;
     }
-
     platform = process.argv[2];
-    this.setupConfigXML()
-      .then(()=>{
-        console.log("cordova config.xml is ready for clive");
-        console.log('exec: cordova run', platform);
-        exec("cordova run", {
-          "stdio": "inherit"
-        });
-        return this.resetConfigXML();
-      })
-      .then(()=>{
-        console.log('clive: done reset config.xml');
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+
+    serverPath = platform === 'ios' ? 'platforms/ios/www' : 'platforms/android/assets/www';
     
+    // Listen to change events on HTML and reload
+    bs.watch("www/**/*.*").on("change", () => {
+      console.log('running cordova prepare');
+      exec("cordova prepare");
+      bs.reload();
+    });
+    // Now init the Browsersync server
+    bs.init({
+      server: serverPath,
+      notify: false
+    }, (err: Error, bs: { options: any }) => {
+      liveUrl = bs.options.getIn(["urls", "external"]);
+      this.setupConfigXML(liveUrl)
+        .then(() => {
+          console.log("cordova config.xml is ready for clive");
+          console.log('exec: cordova run', platform);
+          exec("cordova run", {
+            "stdio": "inherit"
+          });
+          return this.resetConfigXML();
+        })
+        .then(() => {
+          console.log('clive: done reset config.xml');
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    });
+
   }
-  private static setupConfigXML(): Q.Promise<any> {
+  private static setupConfigXML(liveUrl: string): Q.Promise<any> {
     return ConfigXml.setConfigXml(process.cwd(), {
       errorWhenNotFound: true,
-      devServer: "http://9.1.2.3"
+      devServer: liveUrl
     })
   }
   private static resetConfigXML(): Q.Promise<any> {
@@ -47,7 +69,6 @@ class CordovaLiveReload {
       errorWhenNotFound: true
     })
   }
-
 
 }
 
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,3 +1,7 @@
 /// <reference path="node/node.d.ts" />
 /// <reference path="q/Q.d.ts" />
 /// <reference path="x2js/xml2json.d.ts" />
+/// <reference path="browser-sync/browser-sync.d.ts" />
+/// <reference path="chokidar/chokidar.d.ts" />
+/// <reference path="micromatch/micromatch.d.ts" />
+/// <reference path="parse-glob/parse-glob.d.ts" />
