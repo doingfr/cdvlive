225f74a06a379b6bc6582180299cab2ea8e52d7a
Carlos Santana <csantana23@gmail.com>
2016-03-20 13:10:46 -0400

    check if config dir exist instead of file

---
+++ src/config.js
@@ -0,0 +1,49 @@
+/// <reference path="../typings/tsd.d.ts" />
+"use strict";
+var path = require('path');
+var fs = require('fs');
+var CDVLiveConfig = (function () {
+    function CDVLiveConfig() {
+        this.home = path.join(process.env.HOME || process.env.USERPROFILE || process.env.HOMEPATH, '.cdvlive');
+        this.file = path.join(this.home, 'config.json');
+        this.load();
+    }
+    CDVLiveConfig.prototype.load = function () {
+        if (fs.existsSync(this.file)) {
+            this.data = require(this.file);
+        }
+        else {
+            this.data = {};
+        }
+        return this;
+    };
+    CDVLiveConfig.prototype.save = function () {
+        if (!this.data) {
+            return this;
+        }
+        try {
+            if (!fs.existsSync(this.home)) {
+                fs.mkdirSync(this.home);
+            }
+            fs.writeFileSync(this.file, JSON.stringify(this.data, null, 2));
+        }
+        catch (e) {
+            console.error('Unable to save settings file: ' + e);
+        }
+    };
+    CDVLiveConfig.prototype.get = function (k) {
+        return this.data[k];
+    };
+    CDVLiveConfig.prototype.set = function (k, v) {
+        if (!this.data) {
+            this.data = {};
+        }
+        this.data[k] = v;
+        this.save();
+    };
+    //known keys
+    CDVLiveConfig.IP_ADDRESS = 'IP_ADDRESS';
+    return CDVLiveConfig;
+}());
+module.exports = CDVLiveConfig;
+//# sourceMappingURL=config.js.map
---
+++ src/config.ts
@@ -0,0 +1,55 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+import path = require('path');
+import fs = require('fs');
+
+class CDVLiveConfig {
+  private data: any;
+  private home: string = path.join(process.env.HOME || process.env.USERPROFILE || process.env.HOMEPATH, '.cdvlive');
+  private file: string = path.join(this.home, 'config.json')
+
+  constructor() {
+    this.load();
+  }
+
+  public load(): CDVLiveConfig {
+    if (fs.existsSync(this.file)) {
+      this.data = require(this.file);
+    } else {
+      this.data = {};
+    }
+    return this;
+  }
+
+  public save(): CDVLiveConfig {
+    if (!this.data) {
+      return this;
+    }
+    try {
+      if (!fs.existsSync(this.home)) {
+        fs.mkdirSync(this.home);
+      }
+      fs.writeFileSync(this.file, JSON.stringify(this.data, null, 2));
+    } catch (e) {
+      console.error('Unable to save settings file: ' + e);
+    }
+  }
+
+  public get(k: string): any {
+    return this.data[k];
+  }
+
+  public set(k: string, v: any) {
+    if (!this.data) {
+      this.data = {};
+    }
+    this.data[k] = v;
+
+    this.save();
+  }
+  
+  //known keys
+  public static IP_ADDRESS: string = 'IP_ADDRESS';
+
+}
+export = CDVLiveConfig;
